ec3c6e3209a77ff87cc9b34df46660e4a58c6e3c
David Taing <adavidtaing@gmail.com>
2022-08-30 23:37:40 +1000

    Revert "Remove Unnessary Wrapper Div"
    This reverts commit 7918427418f41b5e95c490c7446c5937a2c4411e.

--- pages/login.tsx
+++ pages/login.tsx
@@ -80,47 +80,49 @@ const Logo = ({ className }: { className: string }) => {
 
 const Login: NextPage = () => {
   return (
-    <div className="dashboard-container">
-      <div className="dashboard bg-white h-full w-full px-8 grid grid-cols-12 gap-8 rounded-lg">
-        <div className="col-start-1 col-end-7 p-8">
-          <Logo className="mt-16" />
-          <h1 className="mt-12 text-4xl leading-none font-semibold">Login</h1>
-          <form className="mt-8">
-            <FormField label="Email:" inputId="email" inputType="email" />
-            <FormField
-              label="Password:"
-              inputId="password"
-              inputType="password"
-            />
+    <div className="h-screen">
+      <div className="dashboard-container">
+        <div className="dashboard bg-white h-full w-full px-8 grid grid-cols-12 gap-8 rounded-lg">
+          <div className="col-start-1 col-end-7 p-8">
+            <Logo className="mt-16" />
+            <h1 className="mt-12 text-4xl leading-none font-semibold">Login</h1>
+            <form className="mt-8">
+              <FormField label="Email:" inputId="email" inputType="email" />
+              <FormField
+                label="Password:"
+                inputId="password"
+                inputType="password"
+              />
 
-            <div className="mt-4 h-3.5 flex justify-between items-center">
-              <LabeledCheckbox label="Remember Me" inputId="remember-me" />
-              <div>
-                <a
-                  className="text-sm inline-block leading-none text-sky-600 hover:text-sky-700"
-                  href="/forgot-password"
-                >
-                  Forgot your password?
-                </a>
+              <div className="mt-4 h-3.5 flex justify-between items-center">
+                <LabeledCheckbox label="Remember Me" inputId="remember-me" />
+                <div>
+                  <a
+                    className="text-sm inline-block leading-none text-sky-600 hover:text-sky-700"
+                    href="/forgot-password"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
               </div>
-            </div>
-            <button
-              className="mt-12 w-full h-8 text-sm leading-none rounded text-slate-50 bg-sky-600 hover:bg-sky-700"
-              type="submit"
-            >
-              Login
-            </button>
-          </form>
-        </div>
-        <div className="w-50 relative -ml-4 -mr-8 col-start-7 col-end-13 rounded-r-lg overflow-hidden">
-          <Image
-            className="transition duration-500 hover:scale-105"
-            src="/oscar-sutton-yihlaRCCvd4-unsplash.jpg"
-            alt="Dog at a Beach by Oscar Sutton"
-            layout="fill"
-            object-fit="cover"
-            priority
-          />
+              <button
+                className="mt-12 w-full h-8 text-sm leading-none rounded text-slate-50 bg-sky-600 hover:bg-sky-700"
+                type="submit"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+          <div className="w-50 relative -ml-4 -mr-8 col-start-7 col-end-13 rounded-r-lg overflow-hidden">
+            <Image
+              className="transition duration-500 hover:scale-105"
+              src="/oscar-sutton-yihlaRCCvd4-unsplash.jpg"
+              alt="Dog at a Beach by Oscar Sutton"
+              layout="fill"
+              object-fit="cover"
+              priority
+            />
+          </div>
         </div>
       </div>
     </div>
