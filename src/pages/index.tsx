29fba827fcb756bfe899ff16fa556a476c5b12b5
David Taing <adavidtaing@gmail.com>
2022-08-31 01:22:37 +1000

    Add Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,27 @@
 import type { NextPage } from "next";
-import Image from "next/image";
+import { Logo } from "../components/atoms/Logo";
+import { Dashboard } from "../components/Dashboard";
+
+// TODO - Redirect if user is not logged in.
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <Dashboard>
+      <div className="col-start-1 col-end-13 p-8">
+        <div className="mt-16">
+          <div className="flex justify-between items-center">
+            <Logo />
+            <button className="h-8 px-4 leading-none rounded border border-stone-300 hover:border-stone-800 text-stone-500 hover:text-stone-800">
+              Log Out
+            </button>
+          </div>
+        </div>
+        <h1 className="mt-12 text-4xl leading-none font-semibold">
+          David&apos;s Super Secret Stuff
+        </h1>
+      </div>
+    </Dashboard>
+  );
 };
 
 export default Home;
