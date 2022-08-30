a308de8633853d6e051698484f27268dffa98d00
David Taing <adavidtaing@gmail.com>
2022-08-31 01:08:59 +1000

    Add Link to Logo

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export const Logo = ({ className }: { className: string }) => {
   return (
     <div className={`${className} text-xl leading-none font-bold`}>
-      <span className="underline">Tee</span>ang
+      <Link href="/">
+        <a>
+          <span className="underline">Tee</span>ang
+        </a>
+      </Link>
     </div>
   );
 };
