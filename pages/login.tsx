03feb0bafc38339f47d7550eefcef6bf0cac8924
David Taing <adavidtaing@gmail.com>
2022-08-30 20:29:04 +1000

    Add Signup and Login Pages

---
+++ pages/login.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+
+const Login: NextPage = () => {
+  return <div></div>;
+};
+
+export default Login;
---
+++ pages/signup.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+
+const Signup: NextPage = () => {
+  return <div></div>;
+};
+
+export default Signup;
