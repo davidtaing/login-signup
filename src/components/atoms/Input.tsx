37ea87c569849770e42a560904090a8c7911d766
David Taing <adavidtaing@gmail.com>
2022-08-31 23:03:53 +1000

    Add Explictit Text Type for PasswordInput
    For better clarity

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -25,7 +25,7 @@ export const PasswordInput = (
     <div className={`${props.className} mt-2 w-full h-8 relative`}>
       <input
         {...props}
-        type={showPassword ? undefined : "password"}
+        type={showPassword ? "text" : "password"}
         className="block px-4 h-full w-full border border-stone-300 rounded outline-blue-400"
       />
       <div className="absolute right-4 top-2 text-stone-300 hover:text-stone-700">
